e9c81de6696088539a6c3b409e31ab1d784ed97a
Shiddarth Sharma <shidd7051@gmail.com>
2026-03-18 21:06:46 +0530

    Delete js files directory

--- js files/about.js
+++
@@ -1,7 +0,0 @@
-    function addToCart(btn){
-  const cardBottom = btn.parentElement;
-  const qtyBox = cardBottom.querySelector(".qty-box");
-
-  btn.style.display = "none";
-  qtyBox.classList.remove("d-none");
-}
