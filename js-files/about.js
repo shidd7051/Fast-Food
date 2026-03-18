e12f7939cd73f1e22cd8c0c8d714e75fc3baa74e
Shiddarth Sharma <shidd7051@gmail.com>
2026-03-18 20:56:58 +0530

    Update about.js

--- js-files/about.js
+++ js-files/about.js
@@ -1,1 +1,8 @@
+    function addToCart(btn){
+  const cardBottom = btn.parentElement;
+  const qtyBox = cardBottom.querySelector(".qty-box");
+
+  btn.style.display = "none";
+  qtyBox.classList.remove("d-none");
+}
 
